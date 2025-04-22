a27b5d79964b56761b6617b2a58023a6e74bdb18
Enzo Camelo <enzocamelo8d@gmail.com>
2025-04-22 13:45:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="fatoriais",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Enzo Camelo",
+    author_email="enzocamelo8d@gmail.com",
+    description="Calcula o fatorial de um número inteiro não negativo.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Enzo-Camelo/simple-package-template.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
